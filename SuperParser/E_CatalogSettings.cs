858860e2d8013eeac30ba5cba9cd7316cbc90c80
Владимир Зимин <vladimir_zimin_2255375@mail.ru>
2020-12-14 14:53:47 +0300

    добавил класс Settings

---
+++ SuperParser/E_CatalogSettings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperParser
+{
+    class E_CatalogSettings
+    {
+        public E_CatalogSettings(int start, int end)
+        {
+            StartPoint = start;
+            EndPoint = end;
+        }
+
+        public string BaseUrl { get; set; } = "https://www.e-katalog.ru/list/189/";
+        public string Postfix { get; set; } = "{CurrentId}";
+        public int StartPoint { get; set; }
+        public int EndPoint { get; set; }
+    }
+}
+
