25b6dcc15f551b5bffcf09c77c90b75512691735
Владимир Зимин <vladimir_zimin_2255375@mail.ru>
2020-12-20 13:15:27 +0300

    фиксы

--- SuperConfigurator/MainWindow.xaml.cs
+++ SuperConfigurator/MainWindow.xaml.cs
@@ -12,7 +12,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Collections.Generic;
 using SuperParser;
 
 namespace SuperConfigurator
@@ -30,28 +29,7 @@ namespace SuperConfigurator
             InitializeComponent();
         }
         public void Parser_OnComplited(object o) { MessageBox.Show("Работа завершена!"); }
-        private void BaseUpdate_Click(object sender, RoutedEventArgs e)
-        {
-            //System.Windows.Forms.Application.EnableVisualStyles();
-            //System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
-            //System.Windows.Forms.Application.Run(new Form1());
-            //Form1 form = new Form1();
-            //parser_E_Catalog.Settings = new E_CatalogSettings();
-            Warning w = new Warning();
-            Visibility = Visibility.Hidden;
-            w.ShowDialog();
-            Visibility = Visibility.Visible;
-            //parser_E_Catalog.Start();
-            //Change();
-        }
-
-        private void BuildPC_Click(object sender, RoutedEventArgs e)
-        {
-            Configurator conf = new Configurator();
-            Visibility = Visibility.Hidden;
-            conf.ShowDialog();
-            Visibility = Visibility.Visible;
-        }
+        
 
         private void BaseUpdate1_MouseDown(object sender, MouseButtonEventArgs e)
         {
@@ -68,21 +46,6 @@ namespace SuperConfigurator
             conf.ShowDialog();
             Visibility = Visibility.Visible;
         }
-        //public async void Change()
-        //{
-        //    if (ProgressBar.Value == 14)
-        //        return;
-        //    else
-        //    {                
-        //        if (parser_E_Catalog.IsActive)
-        //            Progress();
-        //        await Task.Delay(1000);
-        //        Change();
-        //    }
-        //}
-        //public void Progress()
-        //{
-        //    ProgressBar.Value++;
-        //}
+       
     }
 }
